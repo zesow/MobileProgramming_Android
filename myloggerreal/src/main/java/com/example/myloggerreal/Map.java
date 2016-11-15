36799b96b570951be2a2387271d7e48672515602
zesow <anstkd07@gmail.com>
2016-11-16 00:11:25 +0900

    진짜 Logger2 과제 2  1. 하고 있는 일,메모,위치 sqlite에 저장하는 것까지 완료.  2. 데이터베이스 긁어와서 통계로 보여주는 것까지 완료.  3. 데이터베이스 긁어와서 지도에 마커 찍어주고 누르면 메모내용 출력 완료.

--- myloggerreal/src/main/java/com/example/myloggerreal/Map.java
+++ myloggerreal/src/main/java/com/example/myloggerreal/Map.java
@@ -1,63 +1,95 @@
 package com.example.myloggerreal;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.util.DisplayMetrics;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-import java.text.NumberFormat;
 import java.util.ArrayList;
 
 
 public class Map extends Activity implements OnMapReadyCallback {
 
     static final LatLng SEOUL = new LatLng(37.56, 126.97);
-    private GoogleMap mMap;
-    Marker selectedMarker;
-    View marker_root_view;
-    TextView tv_marker;
+    private GoogleMap googleMap;
+
+    ArrayList<MarkerItem> mkArrayList;
+    ArrayList<LatLng> latLng;
 
     @Override
     public void onMapReady(final GoogleMap map) {
-        mMap = map;
+        googleMap = map;
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            return;
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+
+            Toast.makeText(getApplicationContext(),
+                    "??", Toast.LENGTH_LONG).show();
         }
 
-        mMap.setMyLocationEnabled(true);
 
-        Marker seoul = mMap.addMarker(new MarkerOptions().position(SEOUL)
-                .title("Seoul"));
-        mMap.setOnMarkerClickListener((GoogleMap.OnMarkerClickListener) this);
-        mMap.setOnMapClickListener((GoogleMap.OnMapClickListener) this);
+        googleMap.setMyLocationEnabled(true);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom( SEOUL, 15));
+        latLng=new ArrayList<LatLng>();
 
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        for(int i=0;i<mkArrayList.size();i++){
+            double longitude=mkArrayList.get(i).getLon();
+            double latitude=mkArrayList.get(i).getLat();
+            LatLng position=new LatLng(latitude,longitude);
 
+            latLng.add(position);
+
+
+            googleMap.addMarker(
+                    new MarkerOptions()
+                        .position(new LatLng(latitude,longitude))
+                        .title(mkArrayList.get(i).getContent())
+            );
+
+        }
+        /*googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+
+            public boolean onMarkerClick(Marker marker) {
+                String text = "[마커 클릭 이벤트] latitude ="
+                        + marker.getPosition().latitude + ", longitude ="
+                        + marker.getPosition().longitude;
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG)
+                        .show();
+                return false;
+            }
+        });*/
+        for(int i=0;i<latLng.size()-1;i++){
+
+            googleMap.addPolyline(new PolylineOptions().add(latLng.get(i),latLng.get(i+1)).width(5).color(Color.RED));
+
+        }
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom( SEOUL, 15));
+
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +99,13 @@ public class Map extends Activity implements OnMapReadyCallback {
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.mapFrag);
         mapFragment.getMapAsync(this);
+
+        final DBHelperReal dbHelper = new DBHelperReal(getApplicationContext(), "doing6.db", null, 1);
+
+        mkArrayList=dbHelper.getMarker();
+
+        //String s=mkArrayList.get(0).content+" "+mkArrayList.get(0).lat+" "+mkArrayList.get(0).lon;
+        //Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
     }
 
 }
